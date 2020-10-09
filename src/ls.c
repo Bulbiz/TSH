31921f388276cab1cc2493437b87c9a240e0307a
Antoine Jourdain <jourdain.antoine@gmail.com>
2020-10-10 00:13:50 +0200

    Implémentation basique de ls

---
+++ src/ls.c
@@ -0,0 +1,41 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int lsPath(const char *path) {
+    DIR *d = opendir(path);
+    if(!d) {
+        perror("opendir");
+        return -1;
+    }
+
+    struct dirent *dir;
+    while((dir = readdir(d))) {
+        printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+
+    return 0;
+}
+
+int ls(int argc, char *argv[]) {
+    int returnValue = 0;
+
+    if(argc == 0) {
+        if(lsPath(".") != 0) {
+            return -1;
+        }
+    } else {
+        for(int i = 0; i < argc; i++) {
+            printf("%s:\n", argv[i]);
+            if(lsPath(argv[i]) != 0) {
+                returnValue = -1;
+            }
+            printf("\n");
+        }
+    }
+
+    return returnValue;
+}
