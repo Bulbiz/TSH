f5a226da8754fd9683501bb9cae060fe9cf57d91
PHOL ASA remy <remy.phol@etu.univ-paris-diderot.fr>
2020-10-16 15:08:14 +0200

    Pwd

--- src/pwd.c
+++ src/pwd.c
@@ -2,42 +2,61 @@
 #include <stdlib.h> 
 #include <stdio.h>
 #include <string.h>
+#include "print.c"
 
-#define TRUE 1
-#define FALSE 0
+#define _POSIX_C_SOURCE 200112L
 #define SIZE 20000
 
-int isInTar();
-char * pwd ();
-void setUpForTarEnter();
-void setUpForTarExit();
+/* Create a char * that contains the PWD */
+char * getPWD (){
+    char * path = (char *) malloc (sizeof(char) * SIZE);
+    strcpy(path,getenv("PWD"));
+    return path;
+}
 
-//FIXME : change printf with the personal print
-char * pwd (){
-    char * path = (char *) malloc (sizeof (char) * SIZE);
-    char * pathTar = getenv("PWDforTar");
-    (isInTar() == TRUE) ? printf("%s\n",strcat(getcwd(path, SIZE), pathTar)) : printf ("%s\n",getcwd(path, SIZE));
+/* Print the values of environment variable PWD */
+void pwd (){
+    char * path = getPWD();
+    strcat(path,"\n\0");
+    print(path);
     free(path);
 }
 
-int isInTar (){
-    if (getenv("PWDforTar") == NULL )
-        return FALSE;
+/* Tell if a file is a tar, auxiliary function */
+int isTar (char * name){
+    int length = strlen(name);
+    if (length <= 4)
+        return 0;
+
+    //Get the extention of the name
+    char * extention = & name [length-4];
+    if (strcmp(extention,".tar") == 0)
+        return 1;
+    return 0;
+}
+
+/* Tell if the path is in a tar,
+   Warning ! the path will be modified so it will be unuseable after ! */
+int isInTar (char * path){
+    char * decompose = strtok(path, "/");
+    while (decompose != NULL){
+        if (isTar(decompose) == 1)
+            return 1;
+        decompose = strtok (NULL,"/");
+    }
+    return 0;
+}
+
+/*
+int main (){
+    char * test = malloc (sizeof(char) * 100);
+    //strcpy (test,"a/b.tar/c/toto");
+    //strcpy (test,"a/b/c/toto");
+
+    if (isInTar(test) == 1)
+        print("This Path is in a Tar");
     else
-        return TRUE;
+        print("This Path is NOT in a Tar");
     
 }
-
-/*Set up when entering a tar */
-void setUpForTarEnter (){
-    if(putenv ("PWDforTar") != 0)
-        perror("PWDforTar");
-    if(setenv ("PWDforTar","/",1) != 0)
-        perror("PWDforTar");
-}
-
-/*Set up when exit a tar */
-void setUpForTarExit (){
-    if(unsetenv ("PWDforTar") != 0)
-        perror("Environment variable PWDforTar could'nt been destroyed");
-}
+*/
