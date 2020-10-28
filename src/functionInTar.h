047ef8b0cc346ba0e99faa615bef40758e0fd774
Pholasa Remy <pholasa2@outlook.fr>
2020-10-28 11:14:48 +0100

    Ajout dans le .h

--- src/functionInTar.h
+++ src/functionInTar.h
@@ -8,5 +8,5 @@ void passContent (int fd, struct posix_header * header);
 int getHeader(int fd, struct posix_header *header);
 void passArchive(int fd);
 int searchFile (int fd,struct posix_header * buf, char * name);
-
+int pathExist (int fd, char * rep);
 #endif
