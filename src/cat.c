ea0ac7cce399105e0133f6d98e09ed1653eb3716
Thierry <chhoathierryprojet@gmail.com>
2020-10-30 15:46:20 +0100

    ajout de la fonction cat globale + carInTar

--- src/cat.c
+++ src/cat.c
@@ -1,54 +1,44 @@
+#include <ctype.h>
+#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-
-//cat without path
-int catOnly (){
-    char * save = (char *) malloc (sizeof(char)* 512);
-    ssize_t sizeMessage = read(STDIN_FILENO, save, 512);
-    if( sizeMessage < 0){
-        printf("bug sur la lecture");
-        return -1;
+#include <unistd.h>
+#include <math.h>
+#include "tar.h"
+#include "print.h"
+#include "functionInTar.h"
+#include "pwd.h"
+ 
+void cat (char * path){
+    if(isInTar (path)){
+        char ** pathInTar =  dividePathWithTar (path);
+        catInTar(pathInTar[0], pathInTar[1]);
+    }else{
+        catOusideTar(path);
     }
-
-    while(sizeMessage > 0){
-        if( write(STDOUT_FILENO , save, sizeMessage) < 0){
-            printf("bug sur l'écriture");
-            return -1;
-        }
-
-        sizeMessage = read(STDIN_FILENO, save, 512);
-    }
-    return 0;
 }
 
-int catWithPath(char * path){
-    int fd = open(path, O_RDONLY);
-    char * save = (char *) malloc (sizeof(char)* 512);
-    ssize_t sizeMessage = read(fd, save, 512);
-    if( sizeMessage < 0){
-        printf("bug sur la lecture");
-        return -1;
-    }
-
-    while(sizeMessage > 0){
-        if( write(STDOUT_FILENO , save, sizeMessage) < 0){
-            printf("bug sur l'écriture");
-            return -1;
+void catInTar(char * archive, char * path){
+    int fd = openArchive (archive, O_RDONLY);
+    struct posix_header * p = malloc (512);
+    while(readHeader (fd, p) > 0){
+        if(strcmp (p -> name, path) == 0){
+            print(getContent (fd,p));
         }
-
-        sizeMessage = read(fd, save, 512);
+        else
+            passContent (fd,p);    
     }
-    return 0;
+    print("fichier introuvable");
 }
 
-/*
+void catOusideTar(char * path){
+     
+}
+
 int main(int argc, char * argv[]){
-    //catOnly();
-    //catWithPath("toto.txt");
     return 0;
-}*/
+}
