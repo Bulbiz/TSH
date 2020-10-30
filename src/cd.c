00912619495ce144943afcd1b1a7bf40502fa5e9
Pholasa Remy <pholasa2@outlook.fr>
2020-10-30 21:16:04 +0100

    Mini change

--- src/cd.c
+++ src/cd.c
@@ -10,17 +10,18 @@
 #include "functionInTar.h"
 #include "pwd.h"
 int cdAux (char * path){
-    printf("IN : %s\n", path);
     char ** traitedPath = dividePathWithTar(path);
     int fd = openArchive(traitedPath[0],O_RDONLY);
     if(fd < 0) {return -1;}
     struct posix_header * buf = malloc (BLOCKSIZE);
-    if ((searchFile(fd,buf,traitedPath[1]) == 0) && (buf -> typeflag = '5'))
+    if ((searchFile(fd,buf,traitedPath[1]) == 0) && (buf -> typeflag = '5')){
         setenv("PWD",path,1);
-    else 
+        close(fd);
+        return 0;
+    } else {
         return -1;
-    close(fd);
-    return 0;
+    }
+
 }
 
 void cd (char * arg){
