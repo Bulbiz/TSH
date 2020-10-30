5848bef64b6b885595ef6dfae499c62fa3fc913e
Thierry <chhoathierryprojet@gmail.com>
2020-10-30 16:52:30 +0100

    implementation de la fonction catOutside

--- src/cat.c
+++ src/cat.c
@@ -10,35 +10,43 @@
 #include <math.h>
 #include "tar.h"
 #include "print.h"
-#include "functionInTar.h"
 #include "pwd.h"
- 
-void cat (char * path){
-    if(isInTar (path)){
-        char ** pathInTar =  dividePathWithTar (path);
-        catInTar(pathInTar[0], pathInTar[1]);
-    }else{
-        catOusideTar(path);
-    }
-}
+#include "functionInTar.h"
 
-void catInTar(char * archive, char * path){
+int catInTar(char * archive, char * path){
     int fd = openArchive (archive, O_RDONLY);
     struct posix_header * p = malloc (512);
     while(readHeader (fd, p) > 0){
         if(strcmp (p -> name, path) == 0){
             print(getContent (fd,p));
+            return 0;
         }
         else
             passContent (fd,p);    
     }
-    print("fichier introuvable");
+    print("fichier introuvable\n");
+    return -1;
 }
 
-void catOusideTar(char * path){
-     
+void catOutsideTar(char * path){
+    int pid = fork();
+    switch(pid){
+        case -1 : perror ("cat");break;
+        case 0 : execlp("cat", "cat", path, NULL); break;
+        default : break;
+    }    
+}
+
+void cat (char * path){
+    if(isInTar(path) == 1){
+        char ** pathInTar = (char **) dividePathWithTar (path);
+        catInTar(pathInTar[0], pathInTar[1]);
+    }else{
+        catOutsideTar(path);
+    }
 }
 
 int main(int argc, char * argv[]){
+    cat(relatifToAbsolute ("test/archive.tar/toto"));
     return 0;
 }
