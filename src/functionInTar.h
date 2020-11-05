934e98982f5acbb00a70c7e01759b4639a027cda
Pholasa Remy <pholasa2@outlook.fr>
2020-11-05 18:14:11 +0100

    Ajout de signature dans functionInTar.h

--- src/functionInTar.h
+++ src/functionInTar.h
@@ -8,7 +8,7 @@ void passContent (int fd, struct posix_header * header);
 int getHeader(int fd, struct posix_header *header);
 void passArchive(int fd);
 int searchFile (int fd,struct posix_header * buf, char * name);
-/*Function from TP1 Systeme*/
-int check_checksum(struct posix_header *hd);
-void set_checksum(struct posix_header *hd);
+struct posix_header createBloc0();
+struct posix_header createHeaderFromFile (int fd, char * newName);
+
 #endif
