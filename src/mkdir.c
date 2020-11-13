775e986a220882c714bdd0c260a582e5bc3b2021
Thierry <chhoathierryprojet@gmail.com>
2020-11-13 19:42:07 +0100

    1ere implementation du rmdir dans le tar

--- src/mkdir.c
+++ src/mkdir.c
@@ -23,5 +23,39 @@ int fileInFolderInTar (char * archive, char * path){
             passContent(fd, buf);
     }
     return -1;
+}   
+
+//FIXME : manque la vérification si le path est un dossier ou pas
+int rmdirInTar(char * archive, char * path){
+    if(fileInFolderInTar(archive, path) == -1){
+        int fd = openArchive (archive, O_RDONLY);
+        size_t size = getSizeAfterFile (path, fd);
+        struct posix_header * buf = malloc (512);
+        size_t fileSize = searchFileSize (fd, buf, path);
+        replaceCurseurToStart (fd);
+
+        char * contentAfterFile = (char *) malloc (sizeof(char)* size);
+        contentAfterFile = getContentUntilPathFile(path, fd, size);
+
+        searchFile(fd, buf, path);
+        char * tmp0 = (char *) malloc (sizeof(char)* size + (fileSize));
+        memset(tmp0, 0, size + fileSize);
+        write(fd, tmp0, size + fileSize);
+
+        replaceCurseurToStart (fd);
+        searchFile(fd, buf, path);
+        write (fd, contentAfterFile, size);
+    
+        free(path);
+        free(archive);
+        free(contentAfterFile);
+        free(buf);
+        free(tmp0);
+        close(fd);
+        return 0;
+    }else{
+        print("action impossible, il reste des fichiers dans le dossier");
+        return -1;
+    }
 }
 
