aea6ec1ccbaae2182e68c332465a2485eed14694
Thierry <chhoathierryprojet@gmail.com>
2020-11-13 19:20:11 +0100

    petit oublie dans une signature

--- src/functionInTar.h
+++ src/functionInTar.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONINTAR_H
 #define FUNCTIONINTAR_H
 void print (char * message);
-void replaceCurseurToStart (fd);
+void replaceCurseurToStart (int fd);
 int openArchive (char * pathname, int flags);
 int readHeader (int fd, struct posix_header * buffer);
 char * getContent (int fd, struct posix_header * header);
