170571369f956ae2f374e08af54d46b86e0fab0e
CHHOA Thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2020-12-03 21:14:03 +0100

    supression commentaire

--- src/cp.c
+++ src/cp.c
@@ -115,9 +115,12 @@ int cp (char ** argv){
     int pathName = isInTar(argv[1]);
     int destination = isInTar(argv[2]);
 
-    /*if(isARepertory(argv[2]) == 0){
+    char * buf = malloc ( sizeof(char) * (strlen(argv[2]) + strlen(argv[1] + fileName (argv[1]))));
+    argv[2] = buf;
+
+    if(isARepertory(argv[2]) == 0){
         sprintf(argv[2],"%s/%s", argv[2], argv[1] + fileName (argv[1]));
-    }*/
+    }
 
     if(pathName == 0 && destination == 0)
         return cp1(argv[1], argv[2]);
